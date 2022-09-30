45ef4d0cbacdbf7b1721824da2f7d8b3f5d015f5
Ilya Piskurov <piskryov@gmail.com>
2022-09-30 22:15:24 +0300

    Complete UI part.

---
+++ src/main/java/BorderPaneController.java
@@ -0,0 +1,74 @@
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.util.LinkedHashMap;
+
+public class BorderPaneController {
+
+    public Button putButton;
+    public Button removeButton;
+    public Button replaceButton;
+    public Button defaultButton;
+    public Button clearButton;
+
+    public TextField putKey;
+    public TextField putValue;
+    public TextField removeKey;
+    public TextField removeValue;
+    public TextField replaceKey;
+    public TextField replaceValue;
+
+    private final LinkedHashMap<String, String> hashMap = new LinkedHashMap<>();
+
+    public void handleMouseClickedPutButton(MouseEvent mouseEvent) {
+        String key   = putKey.getText();
+        String value = putValue.getText();
+
+        if (key.equals("") || value.equals("")) {
+            showErrorDialogIfFieldsAreEmpty();
+        } else {
+            hashMap.put(key, value);
+        }
+    }
+
+    public void handleMouseClickedRemoveButton(MouseEvent mouseEvent) {
+        String key   = removeKey.getText();
+        String value = removeValue.getText();
+
+        if (key.equals("") || value.equals("")) {
+            showErrorDialogIfFieldsAreEmpty();
+        } else {
+            hashMap.put(key, value);
+        }
+    }
+
+    public void handleMouseClickedReplaceButton(MouseEvent mouseEvent) {
+        String key   = replaceKey.getText();
+        String value = replaceValue.getText();
+
+        if (key.equals("") || value.equals("")) {
+            showErrorDialogIfFieldsAreEmpty();
+        } else {
+            hashMap.put(key, value);
+        }
+    }
+
+    public void handleMouseClickedClearButton(MouseEvent mouseEvent) {
+        hashMap.clear();
+    }
+
+    public void handleMouseClickedDefaultButton(MouseEvent mouseEvent) {
+        hashMap.put("Ilon", "Mask");
+        hashMap.put("Coca", "Cola");
+    }
+
+    public void showErrorDialogIfFieldsAreEmpty() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error :<");
+        alert.setHeaderText("Ooops, there was an error!!");
+        alert.setContentText("Field 'Key' or 'Value' is empty!");
+        alert.showAndWait();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,8 +10,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("afina.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.getIcons().add(new Image(String.valueOf(getClass().getResource("icon.png"))));
+        primaryStage.setTitle("Afina");
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
 
